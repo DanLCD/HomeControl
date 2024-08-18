e9e6669c1d34124aa14a519c50959d76626a62b5
DanLCD <44991848+DanLCD@users.noreply.github.com>
2024-08-18 00:12:00 -0600

    Support long value characteristic

--- bootstrap.py
+++ bootstrap.py
@@ -1,7 +1,5 @@
 import asyncio
-import sys
-import threading
-from typing import Any, Union
+from typing import Any
 from async_upnp_client.server import UpnpServer, UpnpServerDevice, UpnpServerService, callable_action, create_event_var
 from async_upnp_client.const import DeviceInfo, ServiceInfo
 from async_upnp_client.utils import get_local_ip
@@ -17,6 +15,7 @@ from bless import (
 
 IP = get_local_ip()
 SERVICE_UUID = 'fcb7f125-606c-57cd-924f-3482a9c10323'
+CHARACTERISTIC_UUID = '51FF12BB-3ED8-46E5-B4F9-D64E2FEC021B'
 
 class IoTService(UpnpServerService):
     SERVICE_DEFINITION = ServiceInfo(
@@ -67,66 +66,70 @@ class IoTDevice(UpnpServerDevice):
     SERVICES = [IoTService]
     ROUTES = []
 
-# NOTE: Some systems require different synchronization methods.
-trigger: Union[asyncio.Event, threading.Event]
-if sys.platform in ["darwin", "win32"]:
-    trigger = threading.Event()
-else:
-    trigger = asyncio.Event()
+class BluetoothBeacon(BlessServer):
+    def __init__(self, name: str='Banco de trabajo inteligente', loop: asyncio.AbstractEventLoop | None = None, name_overwrite: bool = False, **kwargs):
+        super().__init__(name, loop, name_overwrite, **kwargs)
 
+        self._charbuff = bytearray()
+        self._charbuff_evt = asyncio.Event()
 
-def read_request(characteristic: BlessGATTCharacteristic, **kwargs) -> bytearray:
-    print(f"Reading {characteristic.value}")
-    return characteristic.value
+    async def wait_read_value(self) -> bytearray:
+        await self._charbuff_evt.wait()
+        return self._charbuff
 
+    async def start(self, **kwargs):
+        await super().start(**kwargs)
 
-def write_request(characteristic: BlessGATTCharacteristic, value: Any, **kwargs):
-    characteristic.value = value
-    print(f"Char value set to {characteristic.value}")
-    if characteristic.value == b"\x0f":
-        print("NICE")
-        trigger.set()
+        await self.add_new_service(SERVICE_UUID)
+
+        # Add a Characteristic to the service
+        char_flags = (
+            GATTCharacteristicProperties.read
+            | GATTCharacteristicProperties.write
+            | GATTCharacteristicProperties.indicate
+        )
+        permissions = GATTAttributePermissions.readable | GATTAttributePermissions.writeable
+        await self.add_new_characteristic(
+            SERVICE_UUID, CHARACTERISTIC_UUID, char_flags, None, permissions
+        )
+
+    def read_request_func(self, characteristic: BlessGATTCharacteristic, **kwargs) -> bytearray:
+        print(f"Reading {characteristic.value}")
+        return characteristic.value
+
+    def write_request_func(self, characteristic: BlessGATTCharacteristic, value: Any, **kwargs):
+        characteristic.value = value
+        print(f"Char value set to {characteristic.value}")
+
+        if characteristic.uuid == CHARACTERISTIC_UUID:
+            if self._charbuff_evt.is_set():
+                self._charbuff.clear()
+                self._charbuff_evt.clear()
+
+            if characteristic.value == b"\x0f":
+                self._charbuff_evt.set()
+            else:
+                self._charbuff.extend(characteristic.value)
 
 
 async def run():
     loop = asyncio.get_event_loop()
-
-    trigger.clear()
     # Instantiate the server
-    my_service_name = "Banco de trabajo inteligente"
-    server = BlessServer(name=my_service_name, loop=loop)
-    server.read_request_func = read_request
-    server.write_request_func = write_request
+    server = BluetoothBeacon(loop=loop)
 
-    # Add Service
-    my_service_uuid = SERVICE_UUID
-    await server.add_new_service(my_service_uuid)
 
-    # Add a Characteristic to the service
-    my_char_uuid = "51FF12BB-3ED8-46E5-B4F9-D64E2FEC021B"
-    char_flags = (
-        GATTCharacteristicProperties.read
-        | GATTCharacteristicProperties.write
-        | GATTCharacteristicProperties.indicate
-    )
-    permissions = GATTAttributePermissions.readable | GATTAttributePermissions.writeable
-    await server.add_new_characteristic(
-        my_service_uuid, my_char_uuid, char_flags, None, permissions
-    )
-
-    print(server.get_characteristic(my_char_uuid))
+    print(server.get_characteristic(CHARACTERISTIC_UUID))
     await server.start()
     print("Advertising")
-    print(f"Write '0xF' to the advertised characteristic: {my_char_uuid}")
-    if isinstance(trigger, threading.Event):
-        trigger.wait()
-    else:
-        await trigger.wait()
+    print(f"Write '0xF' to the advertised characteristic: {CHARACTERISTIC_UUID}")
+
+    value = await server.wait_read_value()
+    print(value)
 
     await asyncio.sleep(2)
     print("Updating")
-    server.get_characteristic(my_char_uuid)
-    server.update_value(my_service_uuid, "51FF12BB-3ED8-46E5-B4F9-D64E2FEC021B")
+    server.get_characteristic(CHARACTERISTIC_UUID)
+    server.update_value(SERVICE_UUID, CHARACTERISTIC_UUID)
     await asyncio.sleep(5)
     await server.stop()
 
